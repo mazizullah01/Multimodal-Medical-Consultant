2db6fa87f752d5d990fc22f8c3dd0f3fa87dd30f
mazizullah01 <m.azizullah420@gmail.com>
2026-07-25 22:20:40 +0500

    feat: add user message payload for MiniMax request

--- brain_of_doc.py
+++ brain_of_doc.py
@@ -24,15 +24,4 @@ messages = [
     }
 ]
 
-# step4: Send message
-
-response = client.messages.create(
-    model="MiniMax-M3",
-    max_tokens=1000,
-    messages=messages,
-)
-
-# step5: Print message
-
-print(response)
 
