df293c3e4ccfda760868e10fcc491b2dfdad7a2f
mazizullah01 <m.azizullah420@gmail.com>
2026-07-27 17:11:40 +0500

    feat: encode image as base6/define local image path4

--- brain_of_doc_groq.py
+++ brain_of_doc_groq.py
@@ -12,3 +12,7 @@ api_key = os .environ.get("GROQ_API_KEY")
 if not api_key :
     raise ValueError("Missing GROQ_API_KEY in .env or environment")
 
+image_path = os.path.join(folder, "sample-image.jpg")
+with open(image_path, "rb") as file:
+    image_data = base64.b64encode(file.read()).decode("utf-8")
+
