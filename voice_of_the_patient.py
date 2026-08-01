08997fd8904bb701c371fe1b474bd0b12c81f517
mazizullah01 <m.azizullah420@gmail.com>
2026-08-01 13:20:38 +0500

    Add audio recording function structure

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -8,3 +8,15 @@ from pydub import AudioSegment
 from io import BytesIO
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def record_audio(file_path, timeout=20, phrase_time_limit=None):
+
+    """"
+    Simplified function to record audio from the microphone and save it as an MP3 file.
+
+    arg:
+    file_path (str): Path to save the recorded audio file.
+    timeout (int): Maximum time to wait for a phrase to start (in seconds).
+    phrase_time_lfimit (int): Maximum time for the phrase to be recorded (in seconds).
+    """
+  
