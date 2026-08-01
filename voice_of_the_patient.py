33905f4802381d7740a148020105e4f135650eda
mazizullah01 <m.azizullah420@gmail.com>
2026-08-01 13:23:31 +0500

    Add audio recording execution

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -36,3 +36,7 @@ def record_audio(file_path, timeout=20, phrase_time_limit=None):
 
         logging.info(f"Audio saved to {file_path}")
 
+audio_filepath="patient_voice_test.mp3"
+record_audio(audio_filepath, timeout=20, phrase_time_limit=10)
+
+
