c934e899a10024e1df2b2c6f3f2a2ac140658c2c
mazizullah01 <m.azizullah420@gmail.com>
2026-08-05 20:22:22 +0500

    feat: transcribe patient voice input

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from voice_of_the_patient import transcribe_patient_voice
 from brain_of_doc_groq  import brain_of_the_doctor
 from voice_of_the_doc import convert_text_to_doctor_audio, play_audio
 
+# Logic + UI
+
+def process_inputs(audio_filepath, image_filepath, video_filepath):
+
+    # User  will ask question in audio and this audio will be convertes to text
+    patient_text = transcribe_patient_voice(audio_filepath)
+
